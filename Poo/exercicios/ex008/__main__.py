540196eced2683f0cfe16ef4716ee973def9ec40
ArturH16 <holandaartur163@gmail.com>
2026-06-20 14:19:28 -0300

    Encapsulamento: Getters, Setters e @Property (Parte2) - Curso Python POO: Aula 11

---
+++ Poo/exercicios/ex008/__main__.py
@@ -0,0 +1,9 @@
+from ex008 import Avaliacao
+from rich import print,inspect
+def main():
+    av1 = Avaliacao("Pedro","Matemática")
+    print(f" {av1.nome} tirou {av1.get_nota()} em {av1.disciplina}")
+    inspect(av1,private=True)
+
+if __name__ == "__main__":
+    main()
